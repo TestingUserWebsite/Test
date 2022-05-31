1a9812b8ed730bb7bae370316625ea9cd8a68eb2
TestingUserWebsite <106564430+TestingUserWebsite@users.noreply.github.com>
2022-05-31 12:12:02 +1200

    Add files via upload

---
+++ OnlineShop.java
@@ -0,0 +1,17 @@
+
+public class OnlineShop {
+	private int[] Inventory= new int[5];
+	int nProduct = 0;
+	
+	public OnlineShop() {
+		this.Inventory = Inventory;
+		this.nProduct = 0;
+	}
+	
+	public addNProduct(int input) {
+		int count =0;
+		if(nProduct <= Inventory.length) {
+			;
+		}
+	}
+}
